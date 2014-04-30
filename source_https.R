4af23daa1fb17bc05dbe62be743c1ba6f981993f
Xanatos24 <xanatos24@gmail.com>
2014-04-30 15:03:51 -0700

    init

---
+++ source_https.R
@@ -0,0 +1,11 @@
+#source github R scripts
+
+source_https <- function(url, ...) {
+  # load package
+  require(RCurl)
+  
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
